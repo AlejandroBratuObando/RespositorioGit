1483c15a662283c86f625568861ec352b5d94cb1
Alumno tarde <Alumno tarde@EQUIPO16>
2025-03-21 17:28:25 +0100

    Prueba2

--- ProyectoPrueba/src/Prueba/Prueba2.java
+++ ProyectoPrueba/src/Prueba/Prueba2.java
@@ -3,7 +3,11 @@ package Prueba;
 public class Prueba2 {
 
 	
-	String hola;
-	int num1;
-	double num2;
+	public static void main (String[] args) {
+		
+	
+	System.out.println("Esta es la segunda clase de prueba");
+	
+	
+	}
 }
