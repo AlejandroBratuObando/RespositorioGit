22b1b4c94966d39829c04b3854c70f992a0cd948
Alumno tarde <Alumno tarde@EQUIPO16>
2025-03-21 17:27:10 +0100

    Prueba-1

--- ProyectoPrueba/src/Prueba/Prueba.java
+++ ProyectoPrueba/src/Prueba/Prueba.java
@@ -5,6 +5,11 @@ public class Prueba {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		System.out.println("Hola mundo version 1");
+		
+		
+		System.out.println("Hola mundo version 2");
 	}
 
 }
---
+++ ProyectoPrueba/src/Prueba/Prueba2.java
@@ -0,0 +1,9 @@
+package Prueba;
+
+public class Prueba2 {
+
+	
+	String hola;
+	int num1;
+	double num2;
+}
